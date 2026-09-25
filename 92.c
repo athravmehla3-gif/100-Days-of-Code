13de04481d2082b293d00e52857b241e6c744c52
athravmehla3-gif <athravmehla3@gmail.com>
2026-09-25 21:26:05 +0300

    Add files via upload

---
+++ 92.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int count[26] = {0};
+
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        count[str[i] - 'a']++;
+
+        if (count[str[i] - 'a'] == 2) {
+            printf("%c", str[i]);
+            break;
+        }
+    }
+
+    return 0;
+}
